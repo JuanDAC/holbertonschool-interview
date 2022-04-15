ab1d4d7a8463e869ae1a1a58340c04f4152e479d
JuanDAC <juanavila12.0308@gmail.com>
2022-04-15 12:25:18 -0500

    [✨] (refactoring all code style)

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 """
-Module that contain a function with a return of a pascal triangle
+Module that contain a function with a return of a pascal triangle.
 """
+
+
+def second_last(matrix):
+    """
+    Return the second last index.
+    """
+    return matrix[len(matrix) - 2]
+
+
+def top_left(matrix, index):
+    """
+    Return the top left index of current value.
+    """
+    return second_last(matrix)[index - 1]
+
+
+def top_right(matrix, index):
+    """
+    Return the top right index of current value.
+    """
+    return second_last(matrix)[index]
 
 
 def pascal_triangle(n):
     """
-    Returns pascal triangle with n height
+    Returns pascal triangle with n height.
     """
     if n <= 0:
         return []
 
     row = list(range(n))
     row[-1] = row[0] = 1
-    pascal = [*pascal_triangle(n - 1), row]
+    triangle = [*pascal_triangle(n - 1), row]
 
     if len(row) < 3:
-        return pascal
+        return triangle
 
     for i in range(1, len(row) - 1):
-        second_last = len(pascal) - 2
-        top_left = pascal[second_last][i - 1]
-        top_right = pascal[second_last][i]
-        row[i] = top_left + top_right
-    return pascal
+        row[i] = top_left(triangle, i) + top_right(triangle, i)
+    return triangle
