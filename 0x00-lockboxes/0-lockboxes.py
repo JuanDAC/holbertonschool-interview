eafedf35abbbb7bc9389a622ecebfb5d5ad3b2a1
JuanDAC <juanavila12.0308@gmail.com>
2022-04-22 00:55:10 -0500

    [🚀] (add event)

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,25 +2,37 @@
 """ Can un lock all """
 
 
+def setUnlock(boxes, unlocked, index=0):
+    """ Set the lock on all boxes """
+    if unlocked[index] or index >= len(boxes):
+        return
+    unlocked[index] = True
+    keys = boxes[index]
+    for key in keys:
+        setUnlock(boxes, unlocked, key)
+
+
 def canUnlockAll(boxes):
     """ Can un lock all """
+    from functools import reduce
+
     if type(boxes) is not list or len(boxes) == 0:
         return False
 
-    unlocked = boxes[0]
-    for box_id, keys in enumerate(boxes):
-        if not keys:
-            if box_id in unlocked:
-                unlocked.append(0)
-            continue
-        for key in keys:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    unlocked = list(map(lambda _: False, range(len(boxes))))
+    setUnlock(boxes, unlocked)
+    return reduce(lambda acum, value: acum and value, unlocked)
 
 
 if __name__ == "__main__":
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
     boxes = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
     print(canUnlockAll(boxes))
