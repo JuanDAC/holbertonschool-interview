922257badabacf786fc8ddaa41fe9e037d4b54e1
JuanDAC <juanavila12.0308@gmail.com>
2022-06-11 20:14:13 -0500

    [🔥] (add version recustion of cacher)

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from typing import List
+
+
+def makeChange(coins: List[int], total: int, acumulated: int = 0, ordered: bool = False):
+    if len(coins) == 0 and total > 0:
+        return -(acumulated + 1)
+
+    if len(coins) == 0:
+        return 0
+
+    if not ordered:
+        coins.sort(reverse=True)
+
+    biggest, *leftover = coins
+
+    if type(total) is not int or type(biggest) is not int:
+        return -1
+
+    if total == 0:
+        return 0
+
+    possibleAmount = (total // biggest)
+
+    if possibleAmount == 0:
+        return makeChange(leftover, total, acumulated)
+
+    nextTotal = (total % biggest)
+    nextChanges = possibleAmount + \
+        makeChange(leftover, nextTotal, acumulated + possibleAmount, ordered)
+    if nextChanges == -1:
+        nextChanges = makeChange(leftover, total, acumulated, ordered)
+    return nextChanges
+
+
+if __name__ == '__main__':
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    print(makeChange([2, 21], 26))
