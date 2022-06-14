9b1b7a4d23fc8018bb87af7acdf966d9f07bb4e0
JuanDAC <juanavila12.0308@gmail.com>
2022-06-13 23:36:03 -0500

    [🔥] (add add palindrome)

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+int palindrome(listint_t **string_left, listint_t **head)
+{
+    listint_t *move = NULL;
+    listint_t *string_right = NULL;
+
+    if (!head || !*head)
+        return (false);
+
+    string_right = *head;
+
+    if (!string_left)
+        move = string_right, string_left = &move;
+
+    if (palindrome(string_left, &(string_right->next)) == 0 && string_right->next != NULL)
+        return (false);
+
+    if ((*string_left)->n != string_right->n)
+        return (false);
+
+    if ((*string_left)->n == string_right->n)
+        *string_left = (*string_left)->next;
+
+    return (true);
+}
