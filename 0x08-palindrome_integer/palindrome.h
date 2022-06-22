8427a73319f08711c926eef3ddcf275c8c252740
JuanDAC <juanavila12.0308@gmail.com>
2022-06-22 00:26:33 -0500

    [🔥] (fix betty )

--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -1,7 +1,7 @@
 #ifndef _PALINDROME_H
 #define _PALINDROME_H
 
-int palindrome(unsigned long n, unsigned long initial, int base, int length);
+int palindrome(unsigned long n, unsigned long initial, int base, int lastBase);
 
 #define is_palindrome(N) palindrome((N), (N), 0, 0)
 
