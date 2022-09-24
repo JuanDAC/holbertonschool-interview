c28973adc9a085f2462d5e12938239575eeb8b82
JuanDAC <juanavila12.0308@gmail.com>
2022-09-24 11:59:45 -0500

    [🚀] (add check cycle list)

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to head of list
+ * @turtle: pointer to turtle
+ * @hare: pointer to hare
+ * @init: flag to initialize turtle and hare
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int _check_cycle(listint_t *list, listint_t *turtle,
+				 listint_t *hare, int init)
+{
+	if (list == NULL)
+		return (0);
+
+	if (!init && hare == turtle)
+		return (1);
+
+	if (init)
+	{
+		turtle = list;
+		hare = list;
+		init = 0;
+	}
+
+	if (hare == NULL || turtle == NULL)
+		return (0);
+
+	return (_check_cycle(
+		list,
+		turtle->next,
+		hare->nextd ? hare->next->next : hare->next,
+		init));
+}
