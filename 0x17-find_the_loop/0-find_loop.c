976f8d47173c879c34d68af5cef86cd582a02837
JuanDAC <juanavila12.0308@gmail.com>
2022-10-06 18:39:12 -0500

    [🐛] (fix style and logic)

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 
-listint_t *find_listint_loop(listint_t *head)
+/**
+ * _find_listint_loop - finds the loop in a linked list
+ * @head: pointer to head of list
+ * @slow: pointer to head of list
+ * Return: address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+listint_t *_find_listint_loop(listint_t *head, listint_t *slow)
 {
-    listint_t *slow = head;
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    while (slow != NULL)
-    {
-        slow = slow->next;
-        if (slow == head)
-            return (slow);
-    }
+	if (slow == NULL)
+		return (NULL);
 
-    return (NULL);
+	if (slow == head)
+		return (slow);
+
+	return (_find_listint_loop(head, slow->next));
 }
--- 0x17-find_the_loop/lists.h
+++ 0x17-find_the_loop/lists.h
@@ -13,14 +13,16 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
-listint_t *find_listint_loop(listint_t *head);
+listint_t *_find_listint_loop(listint_t *head);
+
+#define find_listint_loop(HEAD) _find_listint_loop(HEAD, HEAD)
 
 #endif /* _LISTS_H_ */
