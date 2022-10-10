aa86271677d200faa4127a1e175a1c143350cf30
JuanDAC <juanavila12.0308@gmail.com>
2022-10-10 11:03:56 -0500

    [🐛] (fix validation before print and doc)

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,5 +1,11 @@
 #include "search_algos.h"
 
+/**
+ * print_array - prints an array
+ * @array: array to print
+ * @size: size of the array
+ * Return: void
+ */
 void print_array(int *array, size_t size)
 {
 	if (size == 0)
@@ -15,13 +21,23 @@ void print_array(int *array, size_t size)
 	print_array(array + 1, size - 1);
 }
 
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * @index: index of the array
+ * Return: index where value is located or -1
+ */
 int _advanced_binary(int *array, size_t size, int value, int index)
 {
 	size_t new_size = (size + 1) / 2;
-	printf("Searching in array: ");
-	print_array(array, size);
+
 	if (size == 0)
 		return (-1);
+
+	printf("Searching in array: ");
+	print_array(array, size);
 
 	if (size == 1)
 		return (*array == value ? index : -1);
