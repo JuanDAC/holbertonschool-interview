c49892e297bec9849993ad919e3180b409053eff
JuanDAC <juanavila12.0308@gmail.com>
2022-11-06 16:50:59 -0500

    [⚡️] (add insert)

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,15 +10,15 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = NULL;
 	/*
-    * insert_node:: (NULL, number) -> NULL
-    */
+	 * insert_node:: (NULL, number) -> NULL
+	 */
 	if (!head)
 	{
 		return (NULL);
 	}
 	/*
-    * insert_node:: (*NULL, number) -> NULL
-    */
+	 * insert_node:: (*NULL, number) -> NULL
+	 */
 	if (!*head)
 	{
 		node = malloc(sizeof(listint_t)), node->next = NULL, node->n = number;
@@ -26,8 +26,8 @@ listint_t *insert_node(listint_t **head, int number)
 		return (node);
 	}
 	/*
-    * insert_node:: (*{ n } : *NULL, number) | n > number -> *listint_t
-    */
+	 * insert_node:: (*{ n } : *NULL, number) | n > number -> *listint_t
+	 */
 	if ((*head)->n > number)
 	{
 		node = malloc(sizeof(listint_t)), node->next = NULL, node->n = number;
@@ -35,8 +35,8 @@ listint_t *insert_node(listint_t **head, int number)
 		return (node);
 	}
 	/*
-    * insert_node:: (_: *{ n } : list, number) | n > number -> *listint_t
-    */
+	 * insert_node:: (_: *{ n } : list, number) | n > number -> *listint_t
+	 */
 	if ((*head)->next && (*head)->next->n > number)
 	{
 		node = malloc(sizeof(listint_t)), node->next = NULL, node->n = number;
@@ -44,8 +44,54 @@ listint_t *insert_node(listint_t **head, int number)
 		return (node);
 	}
 	/*
-    * insert_node:: (_ : [*listint_t], number)
-	*               -> insert_node(listint_t, number)
-    */
+	 * insert_node:: (_ : [*listint_t], number)
+	 *               -> insert_node(listint_t, number)
+	 */
 	return (insert_node(&((*head)->next), number));
+}
+
+/**
+ * add_node_begin - Add a new node to the beginning of a
+ * double circular linked list
+ * Arguments:
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ * Returns: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *node = NULL;
+
+	/*
+	 * add_node_begin:: (NULL, str) -> NULL
+	 */
+	if (!list)
+	{
+		return (NULL);
+	}
+	/*
+	 * add_node_begin:: (*NULL, str) -> NULL
+	 */
+	if (!*list)
+	{
+		node = malloc(sizeof(List)), node->str = strdup(str);
+		node->prev = node, node->next = node;
+		*list = node;
+		return (node);
+	}
+	/*
+	 * add_node_begin:: (*{ str } : *NULL, str) -> *list
+	 */
+	if (!(*list)->next)
+	{
+		node = malloc(sizeof(List)), node->str = strdup(str);
+		node->prev = *list, node->next = *list;
+		(*list)->next = node, (*list)->prev = node;
+		return (node);
+	}
+	/*
+	 * add_node_begin:: (_ : [*list], str)
+	 *               -> add_node_begin(list, str)
+	 */
+	return (add_node_begin(&((*list)->next), str));
 }
---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,47 @@
+
+#include "list.h"
+
+/**
+ * Add a new node to the end of a double circular linked list:
+ * Arguments:
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ * Returns: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+    List *node = NULL;
+
+    /*
+     * add_node_end:: (NULL, str) -> NULL
+     */
+    if (!list)
+    {
+        return (NULL);
+    }
+    /*
+     * add_node_end:: (*NULL, str) -> NULL
+     */
+    if (!*list)
+    {
+        node = malloc(sizeof(List)), node->str = strdup(str);
+        node->prev = node, node->next = node;
+        *list = node;
+        return (node);
+    }
+    /*
+     * add_node_end:: (*{ str } : *NULL, str) -> *list
+     */
+    if (!(*list)->next)
+    {
+        node = malloc(sizeof(List)), node->str = strdup(str);
+        node->prev = *list, node->next = *list;
+        (*list)->next = node, (*list)->prev = node;
+        return (node);
+    }
+    /*
+     * add_node_end:: (_ : [*list], str)
+     *               -> add_node_end(list, str)
+     */
+    return (add_node_end(&((*list)->next), str));
+}
