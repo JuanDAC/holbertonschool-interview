93b70d407b33e87fad7cf7714901acd37c0c1c1e
JuanDAC <juanavila12.0308@gmail.com>
2022-11-26 12:29:03 -0500

    [✨] (add perimeter)

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: height of the tree
+ */
+const binary_tree_t *binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height = 0;
+	int right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ * If tree is NULL, return 0
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (1);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (abs(left_height - right_height) <= 1 &&
+		binary_tree_is_avl(tree->left) &&
+		binary_tree_is_avl(tree->right))
+		return (1);
+
+	return (0);
+}
