1ef4b98e1986e4c33d41fafe209cd93c1ef8beff
JuanDAC <juanavila12.0308@gmail.com>
2022-11-26 11:29:59 -0500

    [✨] (add perimeter)

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,70 +1,47 @@
 #!/usr/bin/python3
 
 
-from typing import List, Dict
-
-
-def perimeter(grid: List[List[int]], islands: Dict[str, bool], i: int, j: int, width: int, height: int) -> int:
-    if not grid:
-        return 0
-
-    if grid[i][j] == 1:
-        key = '%d, %d' % (i, j)
-
-        if (islands.get(key, False)):
-            return 0
-
-        current_perimeter = 0
-        islands[key] = True
-
-        measures = [width, height]
-        structures = [grid, islands]
-
-        if (0 if i-1 == -1 else grid[i-1][j]):
-            current_perimeter += perimeter(*structures, i-1, j, *measures)
-        if (0 if i-1 == height else grid[i+1][j]):
-            current_perimeter += perimeter(*structures, i+1, j, *measures)
-        if (0 if j-1 == -1 else grid[i][j-1]):
-            current_perimeter += perimeter(*structures, i, j-1, *measures)
-        if (0 if j-1 == width else grid[i][j+1]):
-            current_perimeter += perimeter(*structures, i, j+1, *measures)
-
-        perimeter_value = int(not (0 if i-1 == -1 else grid[i-1][j])) \
-            + int(not (0 if i-1 == len(grid) else grid[i+1][j])) \
-            + int(not (0 if j-1 == -1 else grid[i][j-1])) \
-            + int(not (0 if j-1 == len(grid[i]) else grid[i][j+1]))
-
-        return current_perimeter + perimeter_value
-
-    return 0
+from typing import List
 
 
 def island_perimeter(grid: List[List[int]]) -> int:
-
-    if not grid:
-        return 0
-
-    x = -1
-    y = -1
-    width = len(grid[0])
-    height = len(grid)
-    while x == -1 and y < height:
-        y += 1
-        try:
-            x = grid[y].index(1)
-        except Exception:
-            x = -1
-    islands = dict()
-    value = perimeter(grid, islands, y, x, width, height)
-    return value
+    """
+    Function to calculate the perimeter of an island
+    Arguments:
+        - grid is a list of list of integers:
+            * 0 represents water
+            * 1 represents land
+            * Each cell is square, with a side length of 1.
+            * Cells are connected horizontally/vertically
+                (not diagonally).
+            * grid is rectangular, with its width and height
+                not exceeding 100
+        - The grid is completely surrounded by water
+        - There is only one island (or nothing).
+        - The island doesn't have “lakes” (water inside that
+            isn't connected to the water surrounding the island).
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
 
 
 if __name__ == "__main__":
     grid = [
         [0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 1, 0, 1, 0, 0],
-        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 0],
         [0, 0, 0, 0, 0, 0]
     ]
     print(island_perimeter(grid))
