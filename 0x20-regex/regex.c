66668386f01f95f72bb796ba3c2a0d879e95d82f
JuanDAC <juanavila12.0308@gmail.com>
2022-12-23 08:31:45 -0500

    [🚑️] (addvance hard)

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -8,21 +8,21 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-    int tmp = 0;
+	int tmp = 0;
 
-    if (*str == '\0' && *pattern == '\0')
-        return (1);
-    if (*str == '\0' && *pattern == '*')
-        return (regex_match(str, pattern + 1));
-    if (*str == '\0' || *pattern == '\0')
-        return (0);
-    if (*str == *pattern || *pattern == '.')
-        return (regex_match(str + 1, pattern + 1));
-    if (*pattern == '*')
-    {
-        if (*(pattern - 1) == *str || *(pattern - 1) == '.')
-            tmp = regex_match(str + 1, pattern);
-        return (regex_match(str, pattern + 1) || tmp);
-    }
-    return (0);
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+	if (*str == '\0' && *pattern == '*')
+		return (regex_match(str, pattern + 1));
+	if (*str == '\0' || *pattern == '\0')
+		return (0);
+	if (*str == *pattern || *pattern == '.')
+		return (regex_match(str + 1, pattern + 1));
+	if (*pattern == '*')
+	{
+		if (*(pattern - 1) == *str || *(pattern - 1) == '.')
+			tmp = regex_match(str + 1, pattern);
+		return (regex_match(str, pattern + 1) || tmp);
+	}
+	return (0);
 }
